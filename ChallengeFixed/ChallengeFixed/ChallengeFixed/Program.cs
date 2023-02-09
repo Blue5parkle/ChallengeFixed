1c6bcc8f74130074530c9a7c7ee7e219647e5828
Joanna Sereta <joanisk@op.pl>
2023-02-09 21:25:47 +0100

    Day 4

--- ChallengeFixed/ChallengeFixed/ChallengeFixed/Program.cs
+++ ChallengeFixed/ChallengeFixed/ChallengeFixed/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Zadanie dzień 4
+
+string name = "Ewa";
+bool female = false;
+int age = 33;
+
+if (female == true)
+{
+    if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else if (name != "Ewa" && age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+}
+else
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+    else if (name == "Ewa")
+    {
+        Console.WriteLine("Czy to ty byłeś Ewą?")
+    }
+}
